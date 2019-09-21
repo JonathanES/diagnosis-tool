5f84cbd2f7a2ce821708ce3cc5c20c7abefbc2b1
JonathanES <jonathan.espiard@epita.fr>
2019-09-21 17:46:27 -0400

    [ADD] Display graph depending on the symtom and the frequency of the correct diagnosis

--- frontend/src/components/Report.js
+++ frontend/src/components/Report.js
@@ -1,54 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from "react-redux";
 import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-  } from 'recharts';
-  
-  const data = [
-    {
-      name: 'Page A', pv: 2400, amt: 2400,
-    },
-    {
-      name: 'Page B', pv: 1398, amt: 2210,
-    },
-    {
-      name: 'Page C', pv: 9800, amt: 2290,
-    },
-    {
-      name: 'Page D', pv: 3908, amt: 2000,
-    },
-    {
-      name: 'Page E', pv: 4800, amt: 2181,
-    },
-    {
-      name: 'Page F', pv: 3800, amt: 2500,
-    },
-    {
-      name: 'Page G', pv: 4300, amt: 2100,
-    },
-  ];
+    BarChart, Bar, Label, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
 const mapStateToProps = state => ({
     report: state.report.report
 });
 
+class Report extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
 
-const Report= ({report}) => ( 
-    <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
-      </BarChart>
-);
+    componentDidMount(){
+        const report = this.props.report;
+        const data = [];
+        for (let key in report){
+            data.push({name: key, pv: report[key]})
+        }
+        this.setState({data: data});
+    }
+
+    render() {
+        return (
+            <BarChart
+                width={650}
+                height={300}
+                data={this.state.data}
+                margin={{
+                    top: 5, right: 0, left: 0, bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis tick={{fontSize: '10px'}} dataKey={"name"} allowDataOverflow={true} >
+                </XAxis>
+                <YAxis tick={{ fontSize: 15 }}>
+                  <Label value="Frequency" angle={-90} position="insideLeft" textAnchor="middle" />
+                </YAxis>
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="pv" fill="#8884d8" barSize={30} />
+            </BarChart>
+        )
+    };
+}
+
 export default connect(mapStateToProps)(Report);
